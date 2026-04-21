11029b68e0a3986f96a35ce12a3abe52c8bb6ae3
Kvarun16 <varunkavethi18@gmail.com>
2026-04-21 18:16:06 +0530

    python basics

---
+++ Python/Firstpgm.py
@@ -0,0 +1,50 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=40
+b=float(a)
+b
+40.0
+c=str(a)
+c
+'40'
+x="i"
+y=int(x)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    y=int(x)
+ValueError: invalid literal for int() with base 10: 'i'
+a="hiiii"
+y=type(a)
+y
+<class 'str'>
+6+9
+15
+print("answer",6+9)
+answer 15
+a=input()
+print(a)
+2
+2
+1
+1
+2
+2
+1
+1
+a=int(input())
+12
+>>> print(a)
+12
+>>> hi
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    hi
+NameError: name 'hi' is not defined
+>>> p=input("hey enter number")
+hey enter number12
+>>> p
+'12'
+>>> p=int(input("enter"))
+enter12
+>>> 12
+12
---
+++ Python/PythonCpding/Basic/calci.py
@@ -0,0 +1,13 @@
+a=int(input("Enter"))
+b=int(input("Enter"))
+print("sum",a+b)
+
+print("diff",a-b)
+
+print("diff",a//b)
+print("prod",a*b)
+
+print("float",a/b)
+print("mod",a%b)
+
+print("pow",a**b)
